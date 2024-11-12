e536c3091a11816a1a4d357b13c9309cb9baeff9
Bipin Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2024-11-12 18:26:26 -0500

    Refactor argument handling and add error management for file reading

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,46 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query:&String =  &args[1];
-    let filename:&String = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err: &str| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
 
-    println!("Searching for {}", query);
-    println!("In file {}",filename);
 
-    let contents: String = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
+    println!("Searching for {}", config.query);
+    println!("In file {}",config.filename);
 
-    println!("With text:\n{}", contents);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents: String = fs::read_to_string(config.filename)?;
+    println!("With text:\n{}", contents);
+    Ok(())
+}
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
