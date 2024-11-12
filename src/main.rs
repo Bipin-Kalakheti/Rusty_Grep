18580ec1ff8ce456dcf1ae8e4c155cd69e94eaf8
Bipin Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2024-11-12 18:42:51 -0500

    Implement file reading and searching functionality with configuration struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+use Rusty_Grep::run;
+use Rusty_Grep::Config;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -21,26 +23,5 @@ fn main() {
     }
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents: String = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contents);
-    Ok(())
-}
-struct Config {
-    query: String,
-    filename: String,
-}
 
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
 
