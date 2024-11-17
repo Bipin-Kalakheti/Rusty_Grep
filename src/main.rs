0cd0b35fd50eaf52eeac2f8630aba7c1c9c516e0
Bipin Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2024-11-17 15:47:30 -0500

    Refactor argument handling in Config struct to use env::Args and improve error messages

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use Rusty_Grep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err: &str| {
+    let config = Config::new(env::args()).unwrap_or_else(|err: &str| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
